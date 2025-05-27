c0f97a00596cdea16d376dc95ac84a0604236ce3
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-05-26 22:03:30 -0300

    feat: chamar componente BarraDePesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
 import Sidebar from './componentes/SideBar'
 
 function App() {
 
   return (
+    <div className='container'>
     <Sidebar />
+    <BarraDePesquisa />
+    </div>
   )
 }
 
